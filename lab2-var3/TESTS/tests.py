ce3dc0421a3d3a05d6e52beab10ee15a49126614
Barnaeva <161695917+Barnaeva@users.noreply.github.com>
2025-04-20 16:04:35 +0400

    +test longest sequence

--- lab2-var3/TESTS/tests.py
+++ lab2-var3/TESTS/tests.py
@@ -1,4 +1,4 @@
-import math
+from scipy.special import gammaincc as igamc
 
 
 def frequency_bitwise_test(sequence: str) -> float:
@@ -31,4 +31,41 @@ def identical_consecutive_bits(sequence: str) -> float:
     v_n = sum(1 for i in range(n-1) if sequence[i] != sequence[i+1])
     p_value=math.erfc((abs(v_n - 2*n*s_n*(1-s_n)))/(2*math.sqrt(2*n)*s_n*(1-s_n)))
 
+    return p_value
+
+import math
+
+
+def longest_tes_sequence(sequence: str) -> float:
+    """
+    The function checks the longest sequence of elements in a block for a given sequence.
+    :param sequence: Binary string
+    :return: P-value
+    """
+    n = len(sequence)
+    v = [0, 0, 0, 0]
+    blocks = [sequence[i:i + 8] for i in range(0, n, 8)]
+    pi = [0.2148, 0.3672, 0.2305, 0.1875]
+
+    for block in blocks:
+        max_block = 0
+        max_val = 0
+        for val in block:
+            if val == "1":
+                max_val += 1
+                max_block = max(max_val, max_block)
+            else:
+                max_val = 0
+        match max_block:
+            case _ if max_block <= 1:
+                v[0] += 1
+            case 2:
+                v[1] += 1
+            case 3:
+                v[2] += 1
+            case _ if max_block >= 4:
+                v[3] += 1
+
+    chi_square = sum(((v[i] - 16 * pi[i]) ** 2) / (16 * pi[i]) for i in range(4))
+    p_value = igamc(3 / 2, chi_square / 2)
     return p_value
--- lab2-var3/main.py
+++ lab2-var3/main.py
@@ -1,9 +1,10 @@
-from TESTS.tests import frequency_bitwise_test, identical_consecutive_bits
+from TESTS.tests import frequency_bitwise_test, identical_consecutive_bits,longest_tes_sequence
 
 def main():
     d="111100101001010001110101010101010"
     print(frequency_bitwise_test(d))
     print(identical_consecutive_bits(d))
+    print(longest_tes_sequence(d))
 
 
 if __name__ == "__main__":
