7d901337e3f97b2e6cdf20b8e6257d5a5bcb3889
Barnaeva <161695917+Barnaeva@users.noreply.github.com>
2025-05-13 16:12:19 +0400

    Update AsymmetricAlg.py

--- lab3-var3/hybrid_cryptosystem/AsymmetricAlg.py
+++ lab3-var3/hybrid_cryptosystem/AsymmetricAlg.py
@@ -4,35 +4,27 @@ from cryptography.hazmat.primitives import serialization, hashes
 
 class AsymmetricAlg:
 
-    def __init__(self, private_key=None, public_key=None):
-        """
-        Initialize encryption handler.
-
-        :param private_key: private key
-        :param public_key: public key
-        """
-        self.__private_key = private_key
-        self.__public_key = public_key
-
-    def generate_keys(self) -> tuple:
+    @staticmethod
+    def generate_keys() -> tuple:
         """
         Generate new RSA key pair.
 
         :return: private_key, public_key
         """
         keys = rsa.generate_private_key(public_exponent=65537, key_size=2048)
-        self.__private_key = keys
-        self.__public_key = keys.public_key()
-        return self.__private_key, self.__public_key
+        private_key = keys
+        public_key = keys.public_key()
+        return private_key, public_key
 
-    def encrypt_key(self, symmetric_key: bytes) -> bytes:
+    @staticmethod
+    def encrypt_key(symmetric_key: bytes, public_key: rsa.RSAPrivateKey) -> bytes:
         """
         Encrypt text with public key.
 
         :param symmetric_key: text to encrypt
         :return: encrypted key
         """
-        encrypted_key = self.__public_key.encrypt(
+        encrypted_key = public_key.encrypt(
             symmetric_key,
             padding.OAEP(
                 mgf=padding.MGF1(algorithm=hashes.SHA256()),
@@ -42,14 +34,15 @@ class AsymmetricAlg:
         )
         return encrypted_key
 
-    def decrypt_key(self, encrypted_key: bytes) -> bytes:
+    @staticmethod
+    def decrypt_key(encrypted_key: bytes, private_key: rsa.RSAPrivateKey) -> bytes:
         """
         Decrypt data with private key.
 
         :param encrypted_key: encrypted bytes
         :return: decrypted key
         """
-        decrypted_key = self.__private_key.decrypt(
+        decrypted_key = private_key.decrypt(
             encrypted_key,
             padding.OAEP(
                 mgf=padding.MGF1(algorithm=hashes.SHA256()),
