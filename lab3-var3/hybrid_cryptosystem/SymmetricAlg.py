0e191d9d0307c2ad77ebed3b11a182db70538119
Barnaeva <161695917+Barnaeva@users.noreply.github.com>
2025-05-13 16:12:01 +0400

    Update SymmetricAlg.py

--- lab3-var3/hybrid_cryptosystem/SymmetricAlg.py
+++ lab3-var3/hybrid_cryptosystem/SymmetricAlg.py
@@ -4,53 +4,54 @@ from cryptography.hazmat.primitives.ciphers import Cipher, algorithms
 
 
 class SymmetricAlg:
-    def __init__(self, key=None):
-        """
-        Initialize symmetric encryption handler
+    @staticmethod
+    def gen_nonce():
+        return os.urandom(16)
 
-        :param key: Optional existing key (32 bytes for ChaCha20)
-        """
-        self.__key = key
-        self.__nonce = os.urandom(16)
-
-    def generate_key(self) -> bytes:
+    @staticmethod
+    def generate_key() -> bytes:
         """
         Generate new random symmetric key
 
         :return: 32-byte key
         """
-        self.__key = os.urandom(32)
-        return self.__key
+        key = os.urandom(32)
+        return key
 
-    def encrypt(self, plaintext: str) -> tuple[bytes, bytes]:
+    @staticmethod
+    def encrypt(plaintext: str, key: bytes, nonce: bytes) -> bytes:
         """
         Encrypt text with ChaCha20
 
         :param plaintext: Text to encrypt
+        :param key: key
+        :param nonce: nonce
         :return: Tuple of (nonce, ciphertext)
         """
-        padder = padding.ANSIX923(32).padder()
+        padder = padding.PKCS7(128).padder()
         padded_data = padder.update(plaintext.encode()) + padder.finalize()
 
-        cipher = Cipher(algorithms.ChaCha20(self.__key, self.__nonce), mode=None)
+        cipher = Cipher(algorithms.ChaCha20(key, nonce), mode=None)
         encryptor = cipher.encryptor()
         ciphertext = encryptor.update(padded_data) + encryptor.finalize()
 
-        return self.__nonce, ciphertext
+        return ciphertext
 
-    def decrypt(self, ciphertext: bytes, nonce: bytes) -> str:
+    @staticmethod
+    def decrypt(ciphertext: bytes, key: bytes, nonce: bytes) -> str:
         """
         Decrypt ChaCha20 message
 
         :param ciphertext: Encrypted data
         :param nonce: Nonce used for encryption
+        :param key: key
         :return: Decrypted string
         """
-        cipher = Cipher(algorithms.ChaCha20(self.__key, nonce), mode=None)
+        cipher = Cipher(algorithms.ChaCha20(key, nonce), mode=None)
         decryptor = cipher.decryptor()
         padded_data = decryptor.update(ciphertext) + decryptor.finalize()
 
-        unpadder = padding.ANSIX923(32).unpadder()
+        unpadder = padding.PKCS7(128).unpadder()
         data = unpadder.update(padded_data) + unpadder.finalize()
 
         return data.decode()
